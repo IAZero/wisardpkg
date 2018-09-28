1332a90812984f57610220d67aac66f9025fb0f3
Aluizio Lima Filho <aluizio777@gmail.com>
2018-09-28 11:53:53 -0300

    Removed using std from wisard_bind

--- src/wisard_bind.cc
+++ src/wisard_bind.cc
@@ -2,8 +2,6 @@
 //__version__
 #include "version.h"
 
-using namespace std;
-
 PYBIND11_MODULE(wisardpkg, m){
     m.attr("__version__") = __version__;
 
@@ -16,16 +14,16 @@ PYBIND11_MODULE(wisardpkg, m){
 
     //sythesizer
     py::class_<Synthesizer>(m, "Synthesizer")
-      .def(py::init<vector<int>>())
+      .def(py::init<std::vector<int>>())
       .def("make", &Synthesizer::make)
     ;
 
     //base to models
     py::class_<Discriminator>(m, "Discriminator")
-      .def(py::init<string>())
+      .def(py::init<std::string>())
       .def(py::init<int,int,py::kwargs>())
-      .def("train", (void (Discriminator::*)(const vector<int>&)) &Discriminator::train)
-      .def("train", (void (Discriminator::*)(const vector<vector<int>>&)) &Discriminator::train)
+      .def("train", (void (Discriminator::*)(const std::vector<int>&)) &Discriminator::train)
+      .def("train", (void (Discriminator::*)(const std::vector<std::vector<int>>&)) &Discriminator::train)
       .def("classify", &Discriminator::getVotes)
       .def("jsonConfig",  &Discriminator::getConfigString)
       .def("json",  &Discriminator::getJSONString)
@@ -33,24 +31,24 @@ PYBIND11_MODULE(wisardpkg, m){
 
     // models
     py::class_<Wisard>(m, "Wisard")
-      .def(py::init<string>())
+      .def(py::init<std::string>())
       .def(py::init<int,py::kwargs>())
-      .def("train", (void (Wisard::*)(const vector<vector<int>>&, const vector<string>&)) &Wisard::train)
-      .def("classify", (py::list (Wisard::*)(const vector<vector<int>>&)) &Wisard::classify)
+      .def("train", (void (Wisard::*)(const std::vector<std::vector<int>>&, const std::vector<std::string>&)) &Wisard::train)
+      .def("classify", (py::list (Wisard::*)(const std::vector<std::vector<int>>&)) &Wisard::classify)
       .def("getMentalImages", &Wisard::getMentalImages)
-      .def("leaveOneOut", (void (Wisard::*)(const vector<int>&, const string&)) &Wisard::untrain)
-      .def("leaveMoreOut", (void (Wisard::*)(const vector<vector<int>>&, const vector<string>&)) &Wisard::untrain)
+      .def("leaveOneOut", (void (Wisard::*)(const std::vector<int>&, const std::string&)) &Wisard::untrain)
+      .def("leaveMoreOut", (void (Wisard::*)(const std::vector<std::vector<int>>&, const std::vector<std::string>&)) &Wisard::untrain)
       .def("jsonConfig", &Wisard::getConfigJSON)
       .def("json", &Wisard::getJSON)
     ;
 
     py::class_<ClusWisard>(m, "ClusWisard")
       .def(py::init<int, float, int, int, py::kwargs>())
-      .def("train", (void (ClusWisard::*)(const vector<vector<int>>&, const vector<string>&)) &ClusWisard::train)
-      .def("train", (void (ClusWisard::*)(const vector<vector<int>>&, map<int, string>&)) &ClusWisard::train)
+      .def("train", (void (ClusWisard::*)(const std::vector<std::vector<int>>&, const std::vector<std::string>&)) &ClusWisard::train)
+      .def("train", (void (ClusWisard::*)(const std::vector<std::vector<int>>&, std::map<int, std::string>&)) &ClusWisard::train)
       .def("trainUnsupervised", &ClusWisard::trainUnsupervised)
-      .def("classify", (py::list (ClusWisard::*)(const vector<vector<int>>&)) &ClusWisard::classify)
-      .def("classifyUnsupervised", (vector<string> (ClusWisard::*)(const vector<vector<int>>&)) &ClusWisard::classifyUnsupervised)
+      .def("classify", (py::list (ClusWisard::*)(const std::vector<std::vector<int>>&)) &ClusWisard::classify)
+      .def("classifyUnsupervised", (std::vector<std::string> (ClusWisard::*)(const std::vector<std::vector<int>>&)) &ClusWisard::classifyUnsupervised)
       .def("getMentalImage", &ClusWisard::getMentalImage)
       .def("getMentalImages", &ClusWisard::getMentalImages)
     ;
