2c9ceef34706d439b37691900624de5ce539548a
Aluizio Lima Filho <aluizio777@gmail.com>
2018-12-19 18:31:59 -0200

    Launging a new version

--- publish.sh
+++ publish.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 rm -rf build tmp wisardpkg.egg-info wisardpkg*.so dist
-make geninclude && \
-make cpptest && \
-IFS='"' read -ra VERSION <<< $(cat src/version.h) && \
-git commit -a -m "Launging a new version" && \
-git tag v${VERSION[1]} && \
 python3 setup.py test && \
 python3 setup.py sdist && \
 python3 -m twine upload -r pypitest dist/* && \
 python3 -m twine upload -r pypi dist/* && \
-git push && \
-git push --tags && \
 echo "Published!!!"
 rm -rf build tmp wisardpkg.egg-info wisardpkg*.so dist
