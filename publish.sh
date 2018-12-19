cc573a842f442e6d1f5591339b514a86a32be891
Aluizio Lima Filho <aluizio777@gmail.com>
2018-12-19 18:27:11 -0200

    Launging a new version

--- publish.sh
+++ publish.sh
@@ -9,5 +9,7 @@ python3 setup.py test && \
 python3 setup.py sdist && \
 python3 -m twine upload -r pypitest dist/* && \
 python3 -m twine upload -r pypi dist/* && \
+git push && \
+git push --tags && \
 echo "Published!!!"
 rm -rf build tmp wisardpkg.egg-info wisardpkg*.so dist
