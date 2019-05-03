171b113fed4109af10221e17eb546f200d2b86f7
Aluizio Lima Filho <aluizio777@gmail.com>
2019-05-03 11:09:23 -0300

    Added regression on the dataset

--- src/data/dataset.cc
+++ src/data/dataset.cc
@@ -9,6 +9,10 @@ public:
     std::ifstream dataFile;
     dataFile.open(filename);
     if(dataFile.is_open()){
+      std::string filetype="";
+      std::getline(dataFile,filetype,'$');
+      bool isReg = filetype[0] == 'R';
+
       while(true){
         if(dataFile.eof()) break;
 
@@ -17,7 +21,13 @@ public:
         int sharp = input.find("#");
         std::string label = Base64::decode(input.substr(0,sharp));
 
-        add(BinInput(input.substr(sharp+1,input.size()-(sharp+1))),label);
+        if(label.size()>0&&isReg){
+          double y = convertToValue<double>(label);
+          add(BinInput(input.substr(sharp + 1, input.size() - (sharp + 1))), y);
+        }
+        else{
+          add(BinInput(input.substr(sharp + 1, input.size() - (sharp + 1))), label);
+        }
       }
       dataFile.close();
     }
@@ -32,11 +42,22 @@ public:
     data.push_back(input);
   }
 
+  void add(const BinInput& input, double y){
+    addY(y);
+    data.push_back(input);
+  }
+
   void add(const std::vector<short>& input){
     add(input,"");
   }
+
   void add(const std::vector<short>& input, const std::string& label){
     addLabel(label);
+    data.push_back(BinInput(input));
+  }
+
+  void add(const std::vector<short>& input, double y){
+    addY(y);
     data.push_back(BinInput(input));
   }
 
@@ -52,6 +73,10 @@ public:
     return labels.at(index);
   }
 
+  const double& getY(int index) const {
+    return Y.at(index);
+  }
+
   const std::vector<int>& getUnlabelIndices() const {
     return unlabelIndices;
   }
@@ -61,15 +86,23 @@ public:
   }
 
   bool isSupervised() const {
-    return data.size() == labels.size();
+    return data.size() == (labels.size()+Y.size());
   }
 
   bool isUnsupervised() const {
-    return labels.size() == 0;
+    return labels.size() == 0 && Y.size() == 0;
   }
 
   bool isSemiSupervised() const {
-    return data.size() > labels.size() && !isUnsupervised();
+    return data.size() > (labels.size()+Y.size()) && !isUnsupervised();
+  }
+
+  bool isRegression() const {
+    return Y.size() > 0 && labels.size() == 0;
+  }
+
+  bool isClassification() const {
+    return labels.size() > 0 && Y.size() == 0;
   }
 
   size_t size() const {
@@ -80,9 +113,22 @@ public:
     std::string filename = prefix + dataset_sufix;
     std::ofstream dataFile;
     dataFile.open(filename, std::ios::app);
+    bool isReg = false;
+    if(isRegression()){
+      dataFile << "R"; // regression
+      isReg = true;
+    }
+    else if (isClassification()){
+      dataFile << "C"; // classification
+    }
+    else {
+      dataFile << "U"; // unsupervised
+    }
+
+    dataFile << "$";
 
     for(unsigned int i=0; i<data.size(); i++){
-      dataFile << (i != 0 ? "." : "") + Base64::encode(labels[i]) + "#" + data[i].data();
+      dataFile << (i != 0 ? "." : "") + Base64::encode( isReg ? convertToBytes(Y[i]) : labels[i]) + "#" + data[i].data();
     }
 
     dataFile.close();
@@ -91,8 +137,15 @@ public:
 private:
   std::vector<int> unlabelIndices;
   std::vector<int> labelIndices;
+
   std::vector<BinInput> data;
   std::unordered_map<int,std::string> labels;
+  std::unordered_map<int, double> Y;
+
+  void addY(double y){
+    Y[data.size()]=y;
+    labelIndices.push_back(data.size());
+  }
 
   void addLabel(std::string label){
     if(label.size()>0) {
