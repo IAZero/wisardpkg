c1d4ef8d4d93c0ff6a3100c55650cce8ca541d0f
Gabriel Guarisa <gabrielguarisa@gmail.com>
2019-05-22 13:19:21 -0300

    Revert "Changing RAM internal names"
    This reverts commit 01e0e0c5a6f13c7f37a6f2508332b0fcce5884ed.

--- src/models/wisard/ram.cc
+++ src/models/wisard/ram.cc
@@ -5,27 +5,27 @@ public:
   RAM(nl::json c){
     ignoreZero = c["ignoreZero"];
     base=c["base"];
-    mapping = c["mapping"].get<std::vector<int>>();
-    checkLimitAddressSize(mapping.size(), base);
+    addresses = c["addresses"].get<std::vector<int>>();
+    checkLimitAddressSize(addresses.size(), base);
 
     RAMDataHandle handle(c["data"].get<std::string>());
-    memory = handle.get(0);
+    positions = handle.get(0);
   }
   RAM(const int addressSize, const int entrySize, const bool ignoreZero=false, int base=2): ignoreZero(ignoreZero), base(base){
     checkLimitAddressSize(addressSize, base);
-    mapping = std::vector<int>(addressSize);
-    generateRandommapping(entrySize);
+    addresses = std::vector<int>(addressSize);
+    generateRandomAddresses(entrySize);
   }
-  RAM(const std::vector<int> mapping, const bool ignoreZero=false, int base=2): mapping(mapping), ignoreZero(ignoreZero), base(base){
-    checkLimitAddressSize(mapping.size(), base);
+  RAM(const std::vector<int> indexes, const bool ignoreZero=false, int base=2): addresses(indexes), ignoreZero(ignoreZero), base(base){
+    checkLimitAddressSize(indexes.size(), base);
   }
 
   int getVote(const BinInput& image) const {
     addr_t index = getIndex(image);
     if(ignoreZero && index == 0)
       return 0;
-    auto it = memory.find(index);
-    if(it == memory.end()){
+    auto it = positions.find(index);
+    if(it == positions.end()){
       return 0;
     }
     else{
@@ -35,9 +35,9 @@ public:
 
   void train(const BinInput& image){
     addr_t index = getIndex(image);
-    auto it = memory.find(index);
-    if(it == memory.end()){
-      memory.insert(it,std::pair<addr_t,content_t>(index, 1));
+    auto it = positions.find(index);
+    if(it == positions.end()){
+      positions.insert(it,std::pair<addr_t,content_t>(index, 1));
     }
     else{
       it->second++;
@@ -46,27 +46,27 @@ public:
 
   void untrain(const BinInput& image){
       addr_t index = getIndex(image);
-      auto it = memory.find(index);
-      if(it != memory.end()){
+      auto it = positions.find(index);
+      if(it != positions.end()){
         it->second--;
       }
   }
 
   std::vector<std::vector<int>> getMentalImage() {
-    std::vector<std::vector<int>> mentalPiece(mapping.size());
+    std::vector<std::vector<int>> mentalPiece(addresses.size());
     for(unsigned int i=0; i<mentalPiece.size(); i++){
       mentalPiece[i].resize(2);
-      mentalPiece[i][0] = mapping[i];
+      mentalPiece[i][0] = addresses[i];
       mentalPiece[i][1] = 0;
     }
 
-    for(auto j=memory.begin(); j!=memory.end(); ++j){
+    for(auto j=positions.begin(); j!=positions.end(); ++j){
       if(j->first == 0) continue;
       const std::vector<int> address = convertToBase(j->first);
       for(unsigned int i=0; i<mentalPiece.size(); i++){
         if(mentalPiece[i].size() == 0){
           mentalPiece[i].resize(2);
-          mentalPiece[i][0] = mapping[i];
+          mentalPiece[i][0] = addresses[i];
           mentalPiece[i][1] = 0;
         }
         if(address[i] > 0){
@@ -86,44 +86,40 @@ public:
   }
 
   std::string getData() const {
-    RAMDataHandle handle(memory);
+    RAMDataHandle handle(positions);
     return handle.data(0);
   }
 
   void setMapping(std::vector<std::vector<int>>& mapping, int i) const {
-    int size = mapping.size();
+    int size = addresses.size();
     mapping[i].resize(size);
     for(int j=0; j<size; j++) {
-      mapping[i][j] = mapping[j];
+      mapping[i][j] = addresses[j];
     }
   }
 
   int getAddressSize(){
-    return mapping.size();
-  }
-
-  int getTupleSize(){
-    return mapping.size();
+    return addresses.size();
   }
 
   long getsizeof() const{
     long size = sizeof(RAM);
-    size += mapping.size()*sizeof(addr_t);
-    size += memory.size()*(sizeof(addr_t)+sizeof(content_t));
+    size += addresses.size()*sizeof(addr_t);
+    size += positions.size()*(sizeof(addr_t)+sizeof(content_t));
     return size;
   }
 
   ~RAM(){
-    mapping.clear();
-    memory.clear();
+    addresses.clear();
+    positions.clear();
   }
 
 protected:
   addr_t getIndex(const BinInput& image) const{
     addr_t index = 0;
     addr_t p = 1;
-    for(unsigned int i=0; i<mapping.size(); i++){
-      int bin = image[mapping[i]];
+    for(unsigned int i=0; i<addresses.size(); i++){
+      int bin = image[addresses[i]];
       checkPos(bin);
       index += bin*p;
       p *= base;
@@ -133,13 +129,13 @@ protected:
 
 
 private:
-  ram_t memory;
-  std::vector<int> mapping;
+  std::vector<int> addresses;
+  ram_t positions;
   bool ignoreZero;
   int base;
 
   const std::vector<int> convertToBase(const int number) const{
-    std::vector<int> numberConverted(mapping.size());
+    std::vector<int> numberConverted(addresses.size());
     int baseNumber = number;
     for(unsigned int i=0; i<numberConverted.size(); i++){
       numberConverted[i] = baseNumber % base;
@@ -162,9 +158,9 @@ private:
     }
   }
 
-  void generateRandommapping(int entrySize){
-    for(unsigned int i=0; i<mapping.size(); i++){
-      mapping[i] = randint(0, entrySize-1);
+  void generateRandomAddresses(int entrySize){
+    for(unsigned int i=0; i<addresses.size(); i++){
+      addresses[i] = randint(0, entrySize-1);
     }
   }
 };
